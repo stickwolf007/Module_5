05d89b3b69cd3253120f011133ca71db19d3d278
Олег <oleg_stick@mail.ru>
2024-09-20 17:18:10 +0300

    new

---
+++ module_5_3.py
@@ -0,0 +1,76 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.number_of_floors = number_of_floors
+        self.name = name
+
+    def go_to(self, new_floor):
+        if new_floor <= self.number_of_floors:
+            for i in range(new_floor):
+                i += 1
+                print(i)
+        else:
+            print(f'Такого этажа не существует')
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+    def __str__(self):
+        return f'Название {self.name}, количество этажей {self.number_of_floors}.'
+
+    def __eq__(self, other):
+        isinstance(self.number_of_floors, int)
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
