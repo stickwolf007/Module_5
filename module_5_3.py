7cf37f05a36df575c42d57b65c7fdc992bbe036b
Олег <oleg_stick@mail.ru>
2024-09-24 10:40:36 +0300

    new

--- module_5_3.py
+++ module_5_3.py
@@ -19,35 +19,43 @@ class House:
         return f'Название {self.name}, количество этажей {self.number_of_floors}.'
 
     def __eq__(self, other):
-        isinstance(self.number_of_floors, int)
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(self.number_of_floors, int):
+            return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(self.number_of_floors, int):
+            return self.number_of_floors < other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(self.number_of_floors, int):
+            return self.number_of_floors > other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(self.number_of_floors, int):
+            return self.number_of_floors <= other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(self.number_of_floors, int):
+            return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(self.number_of_floors, int):
+            return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(self.number_of_floors, int):
+            self.number_of_floors += value
+            return self
 
     def __radd__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(self.number_of_floors, int):
+            self.number_of_floors += value
+            return self
 
     def __iadd__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(self.number_of_floors, int):
+            self.number_of_floors += value
+            return self
 
 
 
